b3533f2275f0b5b246592a6a5df5d8d195f8566e
Thanh Thuat <ngthanhthuat@gmail.com>
2017-03-03 13:58:22 +0900

    handle delegate for MPAVRoutingController class

--- AirplayDetector/ViewController.m
+++ AirplayDetector/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+@import MediaPlayer;
 
-@interface ViewController ()
+@interface ViewController (){
+    MPVolumeView *volumeView;
+    id routerController;
+}
 
 @end
 
@@ -17,12 +21,40 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self setupAirplayMonitoring];
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)setupAirplayMonitoring
+{
+    routerController = [NSClassFromString(@"MPAVRoutingController") new];
+    [routerController setValue:self forKey:@"delegate"];
+    [routerController setValue:[NSNumber numberWithLong:2] forKey:@"discoveryMode"];
+    
+    CGRect rect;
+    rect = self.view.frame;
+    rect.origin.x = rect.origin.y = 0;
+    
+    volumeView = [[MPVolumeView alloc] initWithFrame:rect];
+    [volumeView setShowsVolumeSlider:NO];
+    [self.view addSubview:volumeView];
+    
+}
+
+-(void)routingControllerAvailableRoutesDidChange:(id)arg{
+    
+    NSArray *availableRoutes = [routerController valueForKey:@"availableRoutes"];
+    for (id router in availableRoutes) {
+        NSString *routerName = [router valueForKey:@"routeName"];
+        NSLog(@"routername -%@",routerName);
+    }
+    
 }
 
 
